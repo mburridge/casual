560059af06765fe9d21942bd1018ef42f52583ba
Michael Burridge <mburridge@zyriab.co.uk>
2021-04-16 15:47:10 +0100

    Removes unused code

--- story-teller-casual/src/components/CountDown.js
+++ story-teller-casual/src/components/CountDown.js
@@ -1,17 +1,11 @@
 import { useState, useEffect } from "react"
 
-/**
- * wait the specified number of ms
- **/
-function timeout(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms))
-}
 const logEnd = () => { console.log('end!') }
 
 const CountDown = ({ seconds, callbackWhenFinish = logEnd }) => {
 
 	const [counter, setCounter] = useState(null);
-  
+
 	useEffect(() => {
 		if (counter === null) setCounter(seconds);
 		else if (counter === 0) {
